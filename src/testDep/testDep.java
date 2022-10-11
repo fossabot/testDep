aadb10eb0b3b741f0956d3cf866594f678441ac9
Yassine Ferjani <yferjani@bfigroupe.com>
2022-10-11 16:36:16 +0100

    first commit

---
+++ src/testDep/testDep.java
@@ -0,0 +1,12 @@
+package testDep;
+import assets.org.apache.commons.*;
+import javax.enterprise.deploy.model.J2eeApplicationObject;
+
+public class testDep {
+
+	public static void main(String[] args) {
+
+		System.out.println("test SBOM");
+	}
+
+}
